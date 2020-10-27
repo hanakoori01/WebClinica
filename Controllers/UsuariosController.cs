fe362c03568e03e4458864dd835148e46835ac72
Joshua99SG <64177422+Joshua99SG@users.noreply.github.com>
2020-10-27 17:19:01 -0600

    27/10/2020 - Actualizacion Controller

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -12,23 +12,107 @@ namespace Clinica.Controllers
 {
     public class UsuariosController : Controller
     {
-        public List<UsuarioTipoUsuario> listaUsuarios;
         private readonly DBClinicaAcmeContext _db;
+        List<UsuarioTipoUsuario> listaUsuario = new List<UsuarioTipoUsuario>();
+        List<Usuario> lista = new List<Usuario>();
         public UsuariosController(DBClinicaAcmeContext db)
         {
             _db = db;
         }
 
-        [HttpPost]
-        public ActionResult Eliminar(int id)
+        public List<UsuarioTipoUsuario> listarUsuarios()
         {
-            var Error = "";
+            listaUsuario = (from usuario in _db.Usuario
+                            join _TipoUsuario in _db.TipoUsuario
+                            on usuario.TipoUsuarioId equals _TipoUsuario.TipoUsuarioId
+                            select new UsuarioTipoUsuario
+                            {
+                                UsuarioId = usuario.UsuarioId,
+                                Nombre = usuario.Nombre,
+                                TipoUsuarioNombre = _TipoUsuario.Nombre,
+                                Password = usuario.Password,
+                            }).ToList();
+            return listaUsuario;
+        }
+
+        private void cargarUltimoRegistro()
+        {
+            var ultimoRegistro = _db.Set<Usuario>().OrderByDescending(e => e.UsuarioId).FirstOrDefault();
+            if (ultimoRegistro == null)
+            {
+                ViewBag.ID = 1;
+            }
+            else
+            {
+                ViewBag.ID = ultimoRegistro.UsuarioId + 1;
+            }
+        }
+
+        private void cargarTipoUsuarios()
+        {
+            List<SelectListItem> listaTipoUsuario = new List<SelectListItem>();
+            listaTipoUsuario = (from tipoUsuario in _db.TipoUsuario
+                                orderby tipoUsuario.Nombre
+                                select new SelectListItem
+                                {
+                                    Text = tipoUsuario.Nombre,
+                                    Value = tipoUsuario.TipoUsuarioId.ToString()
+                                }).ToList();
+            ViewBag.ListaTipoUsuario = listaTipoUsuario;
+        }
+
+        public Usuario recuperarUsuario(int id)
+        {
+            Usuario _Usuario = new Usuario();
+            _Usuario = (from usuario in _db.Usuario
+                        where usuario.UsuarioId == id
+                        select new Usuario
+                        {
+                            UsuarioId = usuario.UsuarioId,
+                            Nombre = usuario.Nombre,
+                            TipoUsuarioId = usuario.TipoUsuarioId
+                        }).First();
+
+            return _Usuario;
+        }
+
+        public IActionResult Index()
+        {
+            listaUsuario = listarUsuarios();
+            return View(listaUsuario);
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            cargarTipoUsuarios();
+            cargarUltimoRegistro();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Usuario usuario)
+        {
+            string Error = "";
             try
             {
-                Usuario _Usuario = _db.Usuario
-                   .Where(u => u.UsuarioId == id).First();
-                _db.Usuario.Remove(_Usuario);
-                _db.SaveChanges();
+                if (!ModelState.IsValid)
+                {
+                    return View(usuario);
+                }
+                else
+                {
+                    string password = Utilitarios.CifrarDatos(usuario.Password);
+                    Usuario _usuario = new Usuario();
+                    _usuario.UsuarioId = usuario.UsuarioId;
+                    _usuario.TipoUsuarioId = usuario.TipoUsuarioId;
+                    _usuario.Nombre = usuario.Nombre;
+                    _usuario.Password = password;
+                    _db.Usuario.Add(_usuario);
+                    _db.SaveChanges();
+                }
             }
             catch (Exception ex)
             {
@@ -37,122 +121,28 @@ namespace Clinica.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-
-
-        public List<UsuarioTipoUsuario> listarUsuarios()
-        {
-            listaUsuarios = (from usuario in _db.Usuario
-                             join _TipoUsuario in _db.TipoUsuario
-                             on usuario.TipoUsuarioId equals _TipoUsuario.TipoUsuarioId
-                             select new UsuarioTipoUsuario
-                             {
-                                 UsuarioId = usuario.UsuarioId,
-                                 Nombre = usuario.Nombre,
-                                 TipoUsuarioNombre = _TipoUsuario.Nombre,
-                                 Password = usuario.Password,
-                             }).ToList();
-            return listaUsuarios;
-        }
-
-        public void llenarTipoUsuario()
-        {
-
-            List<SelectListItem> listaTipoUsuario = new List<SelectListItem>();
-
-                listaTipoUsuario = (from tipousuario in _db.TipoUsuario
-                                    select new SelectListItem
-                                    {
-                                        Text = tipousuario.Nombre,
-                                        Value = tipousuario.TipoUsuarioId.ToString()
-                                    }).ToList();
-                //listaTipoUsuario.Insert(0, new SelectListItem
-                //{
-                //    Text = "--Seleccione--",
-                //    Value = ""
-                //});
-            ViewBag.listaTipoUsuario = listaTipoUsuario;
-        }
-        public Usuario recuperarUsuario(int id)
-        {
-            Usuario _Usuario = new Usuario();
-           _Usuario = (from usuario in _db.Usuario
-                               where usuario.UsuarioId == id
-                               select new Usuario
-                               {
-                                   UsuarioId = usuario.UsuarioId,
-                                   Nombre = usuario.Nombre,
-                                   TipoUsuarioId = usuario.TipoUsuarioId
-                               }).First();
-            
-            return _Usuario;
-        }
-        private void cargarUltimoRegistro()
-        {
-            var ultimoRegistro = _db.Set<Usuario>().OrderByDescending(
-                e => e.UsuarioId).FirstOrDefault();
-            if (ultimoRegistro != null)
-            {
-                ViewBag.ID = ultimoRegistro.UsuarioId + 1;
-            }
-            else
-            {
-                ViewBag.ID = 1;
-            }
-        }
-     public IActionResult Index()
-        {
-            List<UsuarioTipoUsuario> listaUsuarios = new List<UsuarioTipoUsuario>();
-            cargarUltimoRegistro();
-            llenarTipoUsuario();
-            listaUsuarios = listarUsuarios();
-            return View(listaUsuarios);
-        }
-        public string Create(Usuario _Usuario)
-        {
-            string rpta = "";
-            try
-            {
-                if (!ModelState.IsValid && _Usuario == null)
-                {
-                    var query = (from state in ModelState.Values
-                                 from error in state.Errors
-                                 select error.ErrorMessage).ToList();
-                    rpta += "<ul class='list-group'>";
-                    foreach (var item in query)
-                    {
-                        rpta += "<li class='list-group-item list-group-item-danger'>";
-                        rpta += item;
-                        rpta += "</li>";
-                    }
-                    rpta += "</ul>";
-                }
-                else
-                {
-                    rpta = "OK";
-                    string pass = Utilitarios.CifrarDatos(_Usuario.Password);
-                    Usuario user = new Usuario();
-                    user.Nombre = _Usuario.Nombre;
-                    user.TipoUsuarioId = _Usuario.TipoUsuarioId;
-                    user.Password = pass;
-                    _db.Usuario.Add(user);
-                    _db.SaveChanges();
-                }
-            }
-            catch (Exception ex)
-            {
-                rpta = ex.Message;
-            }
-            return rpta;
-        }
-        public JsonResult Edit(int? id)
+        public JsonResult Details(int UsuarioId)
         {
             Usuario _usuario = (from u in _db.Usuario
-                                  where u.UsuarioId == id
-                                  select u).DefaultIfEmpty().Single();
+                                where u.UsuarioId == UsuarioId
+                                select u).DefaultIfEmpty().Single();
+            string pass = Utilitarios.DescifrarDatos(_usuario.Password)
+                               + " (" + "Cifrado : " + _usuario.Password + ")";
+            _usuario.Password = pass;
+            return Json(_usuario);
+        }
+
+        [HttpGet]
+        public JsonResult Edit(int UsuarioId)
+        {
+            Usuario _usuario = (from u in _db.Usuario
+                                where u.UsuarioId == UsuarioId
+                                select u).DefaultIfEmpty().Single();
             _usuario.Password = Utilitarios.DescifrarDatos(_usuario.Password);
             return Json(_usuario);
         }
 
+        [HttpPost]
         public string Edited(Usuario _Usuario)
         {
             string rpta = "";
@@ -193,25 +183,15 @@ namespace Clinica.Controllers
             return rpta;
         }
 
-        public JsonResult Details(int? id)
-        {
-            Usuario _usuario = (from u in _db.Usuario
-                                where u.UsuarioId == id
-                                select u).DefaultIfEmpty().Single();
-            string pass = Utilitarios.DescifrarDatos(_usuario.Password)
-                               + " (" + "Cifrado : " + _usuario.Password + ")";
-            _usuario.Password = pass;
-            return Json(_usuario);
-        }
-
-        public IActionResult Delete(int? UsuarioId)
+        [HttpPost]
+        public IActionResult Delete(int UsuarioId)
         {
             var Error = "";
             try
             {
-                Usuario _Usuario = _db.Usuario
-                   .Where(u => u.UsuarioId == UsuarioId).First();
-                _db.Usuario.Remove(_Usuario);
+                Usuario usuario = _db.Usuario
+                             .Where(e => e.UsuarioId == UsuarioId).First();
+                _db.Usuario.Remove(usuario);
                 _db.SaveChanges();
             }
             catch (Exception ex)
