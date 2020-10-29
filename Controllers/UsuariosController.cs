d77e536b9162a3c6b673b182c969f58298ac1900
Joshua99SG <64177422+Joshua99SG@users.noreply.github.com>
2020-10-28 21:10:07 -0600

    Actualizacion

--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Clinica.Models;
+using Clinica.Models.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using WebClinica.Models;
