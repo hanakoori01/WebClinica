3331570f17ee6e72ec87f8e2c3a65912253ca28b
Joshua99SG <64177422+Joshua99SG@users.noreply.github.com>
2020-10-31 22:24:35 -0600

    Con Login

--- wwwroot/js/especializado.js
+++ wwwroot/js/especializado.js
@@ -511,3 +511,29 @@ function verificar() {
     var value = $('.dataTables_filter input').val();
     alert(value);
 }
+
+/* Js Login*/
+var button = document.getElementById('mainButton');
+
+var openForm = function () {
+    button.className = 'active';
+};
+
+var checkInput = function (input) {
+    if (input.value.length > 0) {
+        input.className = 'active';
+    } else {
+        input.className = '';
+    }
+};
+
+var closeForm = function () {
+    button.className = '';
+};
+
+document.addEventListener("keyup", function (e) {
+    if (e.keyCode == 27 || e.keyCode == 13) {
+        closeForm();
+    }
+});
+/* Js Login*/
